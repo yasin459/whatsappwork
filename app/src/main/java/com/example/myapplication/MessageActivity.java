b0edb97b8ee88d8d084a9bfd4c919c925456bf6a
yasin459 <yasinnmoosavi@gmail.com>
2021-03-28 20:29:23 +0430

    sending message to firebase done

--- app/src/main/java/com/example/myapplication/MessageActivity.java
+++ app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -3,13 +3,18 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
@@ -22,10 +27,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class MessageActivity extends AppCompatActivity {
     private TextView username;
     private ImageView imageView;
-
+    private RecyclerView recyclerView;
+    private EditText message;
+    private ImageButton send_btn;
     private Intent intent;
     private  FirebaseUser fUser;
     private DatabaseReference databaseReference;
@@ -36,6 +45,12 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
         imageView = findViewById(R.id.imageview_profile);
         username = findViewById(R.id.username_profile);
+        recyclerView= findViewById(R.id.recyclerView);
+        message=findViewById(R.id.text_sender);
+        send_btn=findViewById(R.id.btn_send);
+
+
+
 
 //        Toolbar toolbar = findViewById(R.id.toolBar);
 //        setSupportActionBar(toolbar);
@@ -69,6 +84,33 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg =message.getText().toString();
+                if (!msg.equals("")){
+                    sendeMessage(fUser.getUid(),userid,msg);
+
+                }
+                else {
+                    Toast.makeText(MessageActivity.this,"you entered nothing",Toast.LENGTH_SHORT).show();
+
+                }
+                message.setText("");
+                
+            }
+        });
+
+
+    }
+
+    private void sendeMessage(String sender, String receiver , String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String ,String> hashMap = new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("receiver",receiver);
+        hashMap.put("message",message);
+        reference.child("Chats").push().setValue(hashMap);
 
 
     }
